928de897ec97b4fc1c0e0872f9661fd84736aabf
Letícia Dupont <leticia.dupont20@gmail.com>
2022-06-27 19:05:00 -0300

    DepartmentList Controller

--- src/main/java/com/example/workshopjavafxjdbc/Main.java
+++ src/main/java/com/example/workshopjavafxjdbc/Main.java
@@ -9,6 +9,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private static Scene mainScene;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
@@ -17,10 +20,14 @@ public class Main extends Application {
         scrollPane.setFitToHeight(true);
         scrollPane.setFitToWidth(true);
 
-        Scene mainScene = new Scene(scrollPane);
+        mainScene = new Scene(scrollPane);
         stage.setTitle("Sample JavaFX Application");
         stage.setScene(mainScene);
         stage.show();
+    }
+
+    public static Scene getMainScene(){
+        return mainScene;
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/workshopjavafxjdbc/MainController.java
+++ src/main/java/com/example/workshopjavafxjdbc/MainController.java
@@ -1,10 +1,18 @@
 package com.example.workshopjavafxjdbc;
 
+import com.example.workshopjavafxjdbc.utils.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,16 +33,35 @@ public class MainController implements Initializable {
 
     @FXML
     public void onMenuItemDepartmentAction(){
-        System.out.println("action");
+        loadView("DepartmentList.fxml");
     }
 
     @FXML
     public void onMenuItemAboutAction(){
-        System.out.println("action");
+        loadView("About.fxml");
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+
+    private synchronized void loadView(String absoluteName){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            VBox newVBox = loader.load();
+
+            Scene mainScene = Main.getMainScene();
+            VBox mainVBOX = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+            Node mainMenu = mainVBOX.getChildren().get(0);
+            mainVBOX.getChildren().clear();
+            mainVBOX.getChildren().add(mainMenu);
+            mainVBOX.getChildren().addAll(newVBox.getChildren());
+
+        } catch (IOException e){
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
